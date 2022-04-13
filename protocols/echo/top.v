906760bd090a4e8fe3221323e7a767360f3a5ddb
lawrie <lawrie.griffiths@ntlword.com>
2022-04-13 10:10:44 +0100

    Added HDMI and uart examples

---
+++ protocols/echo/top.v
@@ -0,0 +1,28 @@
+module top (
+  input  clk_25mhz,
+  input  ftdi_txd,
+  output ftdi_rxd
+);   
+
+  wire rx_valid;
+  wire [7:0] uart_out;
+  
+  uart_rx uart_receive(
+    .clk(clk_25mhz),
+    .resetn(1'b1),
+    .ser_rx(ftdi_txd),
+    .cfg_divider(25000000/115200),
+    .data(uart_out),
+    .valid(rx_valid)
+  );
+
+  uart_tx uart_transmit(
+    .clk(clk_25mhz),
+    .resetn(1'b1),
+    .ser_tx(ftdi_rxd),
+    .cfg_divider(25000000/115200),
+    .data(uart_out),
+    .data_we(rx_valid)
+  );
+
+endmodule
